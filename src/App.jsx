6d26ddf2c90950f8ddd2a7ca429bb26e2a1b2a64
ahmadnadeemwebdev <ahmad@ahmads-MacBook-Pro.local>
2026-01-17 04:31:27 +0500

    Fix Navbar import case

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import { v4 as uuidv4 } from "uuid";
 
 function App() {
